a16af68cb561d2b0f5dea95eaefa9e9125a4aeee
Alex McArther <acmcarther@gmail.com>
2018-09-09 22:45:04 -0700

    Fix include guard

--- experimental/cc_test/demo_dep.h
+++ experimental/cc_test/demo_dep.h
@@ -1,8 +1,8 @@
-#ifndef EXPERIMENTAL_CC_TEST_H_
-#define EXPERIMENTAL_CC_TEST_H_
+#ifndef EXPERIMENTAL_CC_TEST_DEMO_DEP_H_
+#define EXPERIMENTAL_CC_TEST_DEMO_DEP_H_
 
 #include <stdio.h>
 
 void print(int v);
 
-#endif // EXPERIMENTAL_CC_TEST_H_
+#endif // EXPERIMENTAL_CC_TEST_DEMO_DEP_H_
